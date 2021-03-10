8164d84bad3a01dfb9d04d648bca07a82a35190e
RichardSantillan <rsantilla7x@gmail.com>
2021-03-10 13:27:11 -0600

    Stock Landing Page Ticker and Basic Chart

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        brightness: Brightness.dark,
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
